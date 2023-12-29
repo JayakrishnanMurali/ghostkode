dccede2ca7859869ba502523ebf26f3ef6506909
Jayakrishnan M <jayakrishnan1012@gmail.com>
2023-12-29 17:23:34 +0530

    Update favicon.ico, og-bg.png, and hello-world.mdx files, and add GitHub logo to Codify project page

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -4,6 +4,8 @@ import light from "../../public/images/project/codify/light.png";
 import darkGradient from "../../public/images/project/codify/dark-gradient.png";
 import ogImage from "../../public/images/project/codify/og-image.jpg";
 import { ImageCard } from "app/components/img-card";
+import github from "public/github-logo.svg";
+import Link from "next/link";
 
 export default function Page() {
   const imgs: { alt: string; img: StaticImageData }[] = [
@@ -35,7 +37,33 @@ export default function Page() {
 
       <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
 
-      <h2 className="font-medium text-xl mb-1 tracking-tighter">Codify</h2>
+      <div className="flex items-center gap-4">
+        <Link
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://codify.ghostkode.com/"
+        >
+          <h2 className="font-medium hover:underline text-xl mb-1 tracking-tighter">
+            Codify
+          </h2>
+        </Link>
+
+        <Link
+          href="https://github.com/JayakrishnanMurali/codify"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg
+            width="16"
+            height="16"
+            role="img"
+            aria-label="Vercel logo"
+            className="inline-flex mr-1"
+          >
+            <use href="/sprite.svg#github" />
+          </svg>
+        </Link>
+      </div>
 
       <ImageCard imgs={imgs} />
     </section>
