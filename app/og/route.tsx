6d86fcbd4f0c354f2a2e5ad7cf819c8b18bfaf8c
Jayakrishnan M <jayakrishnan1012@gmail.com>
2023-12-30 14:02:47 +0530

    Update font and background image URLs in og/route.tsx

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,15 +1,9 @@
 import { ImageResponse } from "next/og";
 import { NextRequest } from "next/server";
 
-export const runtime = "nodejs";
-
 export async function GET(req: NextRequest) {
   const { searchParams } = req.nextUrl;
   const postTitle = searchParams.get("title");
-  const font = fetch(
-    new URL("../../public/fonts/kaisei-tokumin-bold.ttf", import.meta.url)
-  ).then((res) => res.arrayBuffer());
-  const fontData = await font;
 
   return new ImageResponse(
     (
@@ -21,7 +15,7 @@ export async function GET(req: NextRequest) {
           flexDirection: "column",
           alignItems: "flex-start",
           justifyContent: "center",
-          backgroundImage: "url(https://ghostkode.com/og-bg.png)",
+          backgroundImage: "url(https://www.ghostkode.com/og-bg.png)",
         }}
       >
         <div
@@ -30,7 +24,7 @@ export async function GET(req: NextRequest) {
             marginRight: 190,
             display: "flex",
             fontSize: 130,
-            fontFamily: "Kaisei Tokumin",
+            fontFamily: "monospace",
             letterSpacing: "-0.05em",
             fontStyle: "normal",
             color: "white",
@@ -45,13 +39,6 @@ export async function GET(req: NextRequest) {
     {
       width: 1920,
       height: 1080,
-      fonts: [
-        {
-          name: "Kaisei Tokumin",
-          data: fontData,
-          style: "normal",
-        },
-      ],
     }
   );
 }
