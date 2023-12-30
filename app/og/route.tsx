1ea7ae417deeb5335f5c5300cfa02c8985437b1d
Jayakrishnan M <jayakrishnan1012@gmail.com>
2023-12-30 12:35:55 +0530

    Update dependencies and fix OG image route

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,29 +1,57 @@
 import { ImageResponse } from "next/og";
-// App router includes @vercel/og.
-// No need to install it.
+import { NextRequest } from "next/server";
 
 export const runtime = "edge";
 
-export function GET() {
+export async function GET(req: NextRequest) {
+  const { searchParams } = req.nextUrl;
+  const postTitle = searchParams.get("title");
+  const font = fetch(
+    new URL("../../public/fonts/kaisei-tokumin-bold.ttf", import.meta.url)
+  ).then((res) => res.arrayBuffer());
+  const fontData = await font;
+
   return new ImageResponse(
     (
       <div
         style={{
-          display: "flex",
-          background: "#f6f6f6",
-          width: "100%",
           height: "100%",
+          width: "100%",
+          display: "flex",
           flexDirection: "column",
+          alignItems: "flex-start",
           justifyContent: "center",
-          alignItems: "center",
+          backgroundImage: "url(https://ghostkode.com/og-bg.png)",
         }}
       >
-        <img width="256" height="256" src='"/public/og-bg.png"' />
+        <div
+          style={{
+            marginLeft: 190,
+            marginRight: 190,
+            display: "flex",
+            fontSize: 130,
+            fontFamily: "Kaisei Tokumin",
+            letterSpacing: "-0.05em",
+            fontStyle: "normal",
+            color: "white",
+            lineHeight: "120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          {postTitle}
+        </div>
       </div>
     ),
     {
-      width: 1200,
-      height: 630,
+      width: 1920,
+      height: 1080,
+      fonts: [
+        {
+          name: "Kaisei Tokumin",
+          data: fontData,
+          style: "normal",
+        },
+      ],
     }
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,11 @@
-import { sql } from '@vercel/postgres';
-
 const nextConfig = {
   experimental: {
     ppr: true,
   },
-  async redirects() {
-    if (!process.env.POSTGRES_URL) {
-      return [];
-    }
-
-    const { rows: redirects } = await sql`
-      SELECT source, destination, permanent
-      FROM redirects;
-    `;
-
-    return redirects.map(({ source, destination, permanent }) => ({
-      source,
-      destination,
-      permanent: !!permanent,
-    }));
-  },
   headers() {
     return [
       {
-        source: '/(.*)',
+        source: "/(.*)",
         headers: securityHeaders,
       },
     ];
@@ -42,32 +24,32 @@ const ContentSecurityPolicy = `
 
 const securityHeaders = [
   {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\n/g, ''),
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\n/g, ""),
   },
   {
-    key: 'Referrer-Policy',
-    value: 'origin-when-cross-origin',
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
   },
   {
-    key: 'X-Frame-Options',
-    value: 'DENY',
+    key: "X-Frame-Options",
+    value: "DENY",
   },
   {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
+    key: "X-Content-Type-Options",
+    value: "nosniff",
   },
   {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on',
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
   },
   {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=31536000; includeSubDomains; preload',
+    key: "Strict-Transport-Security",
+    value: "max-age=31536000; includeSubDomains; preload",
   },
   {
-    key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=()',
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=()",
   },
 ];
 
